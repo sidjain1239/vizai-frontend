dc79fd5f89c57aaa1fb6ce8e92fdddd0f9eabf6c
Siddharth Jain <155904393+sidjain1239@users.noreply.github.com>
2025-07-13 14:50:40 +0530

    Delete app/home directory

--- app/home/components/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-const NavBar = () => {
-  return (
-    <div className={styles.navbar}>NavBar</div>
-  )
-}
-
-export default NavBar
